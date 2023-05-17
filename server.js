e754d03ba3f89a69741c7265c15d736519ab190c
bswiley <bswiley@gmail.com>
2023-05-16 20:57:06 -0500

    saved to github from heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { uuid } = require('uuidv4');
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
